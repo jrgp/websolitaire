6cb0e1c1b7d1571c4149249272af7c6ad268ac1a
Joe Gillotti <joe@u13.net>
2014-11-09 20:35:26 -0800

    Initial commit of web solitaire

---
+++ game.js
@@ -0,0 +1,157 @@
+// Joe Gillotti - 11/9/2014
+
+
+/**
+ * Combined state class... 
+ */
+
+function Game() {
+
+  /**
+   * Maintain card state and position in these arrays.
+   */
+
+  this.cards = [];
+  this.deck = [];
+  this.slots = [];
+  this.stacks = []
+
+  /*
+   * Core classes
+   */
+
+  function Slot(type) {
+    this.cards = [];
+    this.type = type
+
+    this.getLastCard = function() {
+      return this.cards[this.cards.length - 1];
+    }
+  }
+
+  // Add card up front
+  Slot.prototype.add = function(card) {
+    // If this is a top slot, allow if the card being 
+    // added is greater than the last in our stack, starting
+    // with Ace
+    if (this.type == 'topslot') {
+      if (this.cards.length == 0 && card.type == 'ace') {
+        this.cards.push(card);
+        return true;
+      }
+      if (this.cards.length > 0 && card.greaterThan(this.getLastCard()) && card.suite == this.getLastCard().suite) {
+        this.cards.push(card);
+        return true;
+      }
+    }
+
+    // If this is a stack, allow if card being added is less
+    // than last in our stack, starting with King. Also alternate
+    // colors
+    else if (this.type == 'stack') {
+      if (this.cards.length == 0 && card.type == 'king') {
+        this.cards.push(card);
+        return true;
+      }
+      if (this.cards.length > 0 && card.lessThan(this.getLastCard()) && card.getColor() != this.getLastCard().getColor()) {
+        this.cards.push(card);
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function Card(suite, type) {
+    this.suite = suite;
+    this.type = type;
+
+    this.getWeight = function() {
+      var comparisons = {};
+      var i = -1;
+      comparisons['ace'] = ++i;
+      for (var j = 2; j <= 10; j++) {
+        comparisons[j] = ++i;
+      }
+      var types = ['jack', 'queen', 'king'];
+      for (var type in types) {
+        comparisons[types[type]] = ++i;
+      }
+      return comparisons[this.type];
+    }
+  }
+
+  Card.prototype.greaterThan = function (othercard) {
+    return this.getWeight() > othercard.getWeight();
+  }
+
+  Card.prototype.lessThan = function (othercard) {
+    return this.getWeight() < othercard.getWeight();
+  }
+
+  Card.prototype.getColor = function() {
+    return {
+      'clubs': 'black',
+      'spades': 'black',
+      'diamonds': 'red',
+      'hearts': 'red',
+    }[this.suite];
+  }
+
+
+  /**
+   * Convenience functions
+   */
+
+  // Check game over
+  this.checkWin = function () {
+    return true;
+  }
+
+  /**
+   * Populate & start game:
+   */
+  this.restart = function() {
+    this.cards = [];
+    this.deck = [];
+    this.slots = [];
+    this.stacks = []
+
+    for (var i = 0; i < 4; i++) {
+      this.slots.push(new Slot('topslot'));
+    }
+
+    for (var i = 0; i < 7; i++) {
+      this.stacks.push(new Slot('stack'));
+    }
+
+    // Populate all our available this.cards
+    var suites = ['clubs', 'diamonds', 'hearts', 'spades'];
+    var types = ['ace', 'jack', 'queen', 'king'];
+    for (var suite in suites) {
+      for (var i = 2; i <= 10; i++) {
+        this.cards.push(new Card(suites[suite], i));
+      }
+
+      for (var type in types) {
+        this.cards.push(new Card(suites[suite], types[type]));
+      }
+    }
+
+    // Shuffle cards... (SugarJS)
+    this.cards = this.cards.randomize()
+
+    // Stacks
+    for (var i = 0; i < 7; i++) {
+      for (var j = 0; j < i+1; j++) {
+        this.stacks[i].cards.push(this.cards.shift())
+      }
+    }
+
+    // Stuff remaining ones into front deck
+    var card;
+    while ((card = this.cards.shift()) != undefined) {
+      this.deck.push(card)
+    }
+  }
+}
---
+++ ui.js
@@ -0,0 +1,177 @@
+// Joe Gillotti - 11/9/2014
+
+/*
+ * Control aspects of UI
+ */
+
+function Ui () {
+  this.game = new Game();
+
+  this.visibleDeckCard = -1;
+
+  function getCardUnicode(card) {
+    // http://en.wikipedia.org/wiki/Standard_52-card_deck#Text
+    if (card.type == 'boxcover')
+      return '&#x1F0A0';
+
+    var numberProgressions = {
+      ace:   '1',
+      king:  'E',
+      queen: 'D',
+      jack:  'C',
+      10:    'A',
+    };
+
+    for (var i = 2; i <= 9; i++) {
+      numberProgressions[i] = i;
+    }
+
+    var suiteProgressions = {
+      diamonds: 'C',
+      hearts:   'B',
+      spades:   'A',
+      clubs:    'D',
+    }
+    
+    return '&#x1F0'+ suiteProgressions[card.suite]+numberProgressions[card.type];
+  }
+
+  this.drawCard = function(card, depth, drag) {
+    depth = depth || 0;
+    drag = drag || false;
+    var text = getCardUnicode(card);
+    var div = $('<div>')
+                .addClass(card.suite)
+                .addClass('card')
+                .html(text)
+                .css('top', (depth*2) + 'px')
+                .css('left', '0');
+    
+    if (drag) {
+      div.draggable({
+        containment: '#content',
+        cursor: 'grab',
+        revert: true
+      });
+      div.data('card', card)
+    }
+
+    return div;
+  }
+
+  this.redraw = function() {
+  
+    // Empty elements...
+    for (var i = 1; i <= 4; i++) {
+      $('#slot'+i).empty();
+    }
+    for (var i = 1; i <= 7; i++) {
+      $('#stack'+i).empty();
+    }
+
+
+    // Put cards in stack on bottom..
+    for (var i = 0; i < 7; i++) {
+      var cards = this.game.stacks[i].cards;
+      var stackwrapper = $('#stack'+(i+1));
+      for (var j = 0; j < cards.length; j++) {
+        var element = this.drawCard(cards[j], j, j == cards.length - 1);
+        element.data('source', 'stacks');
+        element.data('sourceindex', i);
+        element.data('cardindex', j);
+        stackwrapper.append(element)
+      }
+    }
+
+    // Put cards in slots on top
+    for (var i = 0; i < 4; i++) {
+      var cards = this.game.slots[i].cards;
+      var slotwrapper = $('#slot'+(i+1));
+      for (var j = 0; j < cards.length; j++) {
+        var element = this.drawCard(cards[j], j, j == cards.length - 1);
+        element.data('source', 'slots');
+        element.data('sourceindex', i);
+        element.data('cardindex', j);
+        slotwrapper.append(element)
+      }
+    }
+
+    // Deal with deck thing
+    $('#boxcover').append(this.drawCard({type: 'boxcover'}))
+
+    $('#hand').empty()
+    if (this.visibleDeckCard > -1 && this.visibleDeckCard < this.game.deck.length) {
+      $('#hand').append(this.drawCard(this.game.deck[this.visibleDeckCard], 0, true));
+    }
+
+  }
+
+  /*
+   * When you click the deck card thing, show next available card
+   */
+  $('#boxcover').click(_.bind(function() {
+    var next = this.visibleDeckCard + 1;
+    if (next > -1 && next < this.game.deck.length)
+      this.visibleDeckCard = next;
+    else {
+      this.visibleDeckCard = -1;
+      console.log('reverting')
+    }
+    this.redraw();
+  }, this));
+
+  /*
+   * Handle dropping cards onto stacks and slots
+   */
+
+  var configDropHandle = (function(my, dest, j) {
+      $('#'+dest+(j + 1)).droppable({
+        accept: '.card',
+        hoverClass: 'dropping',
+        drop: _.bind(function(e, ui) {
+          
+          var element = ui.draggable;
+
+          var card = element.data('card');
+          var source = element.data('source');
+          var sourceIndex = element.data('sourceindex');
+          var cardIndex = element.data('cardindex');
+
+          if (my.game[dest+'s'][j].add(card)) {
+            my.game[source][sourceIndex].cards.splice(cardIndex, 1);
+
+            ui.draggable.draggable('option', 'revert', false);
+            ui.draggable.draggable('disable');
+
+            if (source == 'deck') {
+              console.log('came from deck')
+            }
+
+            my.redraw();
+          }
+          else {
+            console.log('Invalid move.')
+          }
+        }, this)
+      })
+    })
+
+  // Slots at top
+  for (var i = 0; i < 4; i++) {
+    configDropHandle(this, 'slot', i);
+  }
+
+  // Stacks at bottom
+  for (var i = 0; i < 7; i++) {
+    configDropHandle(this, 'stack', i);
+  }
+
+  /*
+   * Start game and redo everything
+   */
+  this.restart = function() {
+    this.visibleDeckCard = -1;
+    this.game.restart();
+    this.redraw();
+  }
+}
